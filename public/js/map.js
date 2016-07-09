da559acb40c31a1185e0b7eddc6be51b176ff3a4
Dima Girya <DimaGirya1990@gmail.com>
2016-07-09 18:35:17 +0300

    find patch work

--- public/js/map.js
+++ public/js/map.js
@@ -1,11 +1,11 @@
 var app = angular.module("map",[]);
 app.controller("mapController",function ($scope,$http) {
+    $scope.serverStr = "http://localhost:3000";  // for work which localhost server
+    //  $scope.serverStr =  "https://mapcollege.herokuapp.com";  // for work which heroku server
     $scope.mapData = null;
     $scope.classes = null;
     $scope.rooms = null;
     $scope.readyToSendClassStatusUpdate = false;
-    $scope.serverStr = "http://localhost:3000";  // for work which localhost server
-  //  $scope.serverStr =  "https://mapcollege.herokuapp.com";  // for work which heroku server
     $scope.inputFrom = "";
     $scope.inputTo = "";
     $scope.message = "";
@@ -14,6 +14,30 @@ app.controller("mapController",function ($scope,$http) {
         name:"",
         id :-1
     };
+
+    $scope.roomTo = {
+        status:"",
+        name:"",
+        id :-1
+    };
+    $scope.roomFrom = {
+        status:"",
+        name:"",
+        id :-1
+    };
+
+    $scope.bindIdToRoomName = function (roomObject) {
+        var size = $scope.rooms.length;
+        for(var i = 0; i <  size; i++){
+            if( $scope.rooms[i].name === roomObject.name) {
+                roomObject.id =  $scope.rooms[i].id;
+                return;
+            }
+        }
+        roomObject.id = -1;
+    };
+
+
     $scope.bindIdToClassName = function () {
         var size = $scope.classes.length;
         for(var i = 0; i <  size; i++){
@@ -26,8 +50,10 @@ app.controller("mapController",function ($scope,$http) {
         $scope.readyToSendClassStatusUpdate = false;
         $scope.classToReport.id = -1;
     };
+
     $scope.refreshMapData = function () {
         $http.get( $scope.serverStr+"/getMapToDisplay").success(function (data) {
+            console.log(data);
             $scope.mapData = data;
         });
         $http.get( $scope.serverStr+"/getMap").success(function (data) {
@@ -57,12 +83,41 @@ app.controller("mapController",function ($scope,$http) {
             console.log("not ready to send");
         }
     };
-    $scope.sendFindPatchRequest = function(){   //todo
-        $http.get( $scope.serverStr+"/getPath/"+$scope.inputFrom+"/"+$scope.inputTo).success(function () {
-           console.log(data); 
+
+    $scope.sendFindPatchRequest = function(){
+        if($scope.roomTo.id === -1 ||  $scope.roomFrom.id === -1){
+            console.log($scope.roomTo);
+            console.log($scope.roomFrom);
+            return;
+        }
+        $scope.refreshMapData();
+        $http.get( $scope.serverStr+"/getPath/"+ $scope.roomFrom.id+"/"+ $scope.roomTo.id).success(function (data) {
+            console.log(data);
+            var count = 0;
+            angular.forEach(data.path,function (hallway) {
+                angular.forEach($scope.mapData,function (row) {
+                    angular.forEach(row,function (cell) {
+                        count++;
+                        if(hallway == cell.place_id){
+                            cell.status = "inPatch";
+                        }
+                    });
+                });
+            });
+            console.log(count);
+
+
         });
-        $scope.inputFrom = "";
-        $scope.inputTo = "";
+        $scope.roomTo = {
+            status:"",
+            name:"",
+            id :-1
+        };
+        $scope.roomFrom = {
+            status:"",
+            name:"",
+            id :-1
+        };
     };
     $scope.refreshMapData();
 });
